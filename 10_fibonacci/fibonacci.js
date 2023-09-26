8034058cc3939aa5c6c3b2118f583d99814d1ac4
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-09-26 14:00:15 +0300

    Finished all the assignments here

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS!";
+    if (num === 0) return 0;
+
+    let first = 1;
+    let second = 0;
+
+    for (let i = 2: i <= num; i++) {
+        let now = first + second;
+        second = first;
+        first = now;
+    }
+    return first
 
 };
 
