412d090ef8866c498fcd7ed77d85d219b1201d36
Yosef Lachman <46474203+Persuantgecko80@users.noreply.github.com>
2023-09-26 12:27:37 +0300

    New?

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (!Number.isInteger(firstNumber) || (!Number.isInteger(secondNumber))) return "ERROR";
+    if (secondNumber < 0 || firstNumber < 0) return "ERROR";
+    if (firstNumber > secondNumber) {
+        const temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+    let sumOfAll = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sumOfAll += i;
+    }
+    return sumOfAll;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
